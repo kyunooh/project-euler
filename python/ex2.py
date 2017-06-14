4a93fd5525bbf083307bdec49ea17b470e6510a7
Hyeon-Mook Jerry Choi <chm073@gmail.com>
2017-06-14 12:26:05 +0900

    Add ex 2

---
+++ python/ex2.py
@@ -0,0 +1,21 @@
+def get_answer():
+    fibonacci_list = get_fibonacci_list()
+    fibonacci_list_sum = sum(filter(lambda x: x % 2 == 0, fibonacci_list))
+    return fibonacci_list_sum
+
+
+def get_fibonacci_list(under=4000000):
+    fibonacci_list = [1, 2]
+    while True:
+        fibonacci = next_fibonacci(fibonacci_list[-2], fibonacci_list[-1])
+        if fibonacci > under:
+            break
+        fibonacci_list.append(fibonacci)
+
+    return fibonacci_list
+
+
+def next_fibonacci(first, second):
+    return first + second
+
+print(get_answer())
