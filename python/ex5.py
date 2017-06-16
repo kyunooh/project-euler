ba4d77df657a7ceeb3a3b9fb02e5f19122f53c87
Hyeon-Mook Jerry Choi <chm073@gmail.com>
2017-06-16 13:42:01 +0900

    Opimize ex5

--- python/ex5.py
+++ python/ex5.py
@@ -12,11 +12,34 @@ def is_not_smallest_multiple(number):
     return False
 
 
-def get_smallest_multiple():
-    solution_number = 2
-    while is_not_smallest_multiple(solution_number):
-        solution_number += 1
-    return solution_number
+def get_smallest_multiple(end_num=20):
+    primes = []
+    prime_candidate = range(2, end_num)
+    for i in prime_candidate:
+        is_prime = True
+        for j in range(2, i - 1):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(i)
 
+    max_exp_num_list = []
+    for i in primes:
+        p = i
+        while True:
+            exp_num = p * i
+            if exp_num > end_num:
+                max_exp_num_list.append(p)
+                break
+            p = exp_num
+
+    answer = 1
+    for i in max_exp_num_list:
+        answer *= i
+    return answer
+
+
+print(get_answer())
 
 test.running_time(get_answer)
